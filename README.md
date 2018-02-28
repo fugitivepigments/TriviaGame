8be1c948a4fe071e2f557e09d24a8ef01d705335
Case Western Reserve University <andrew.mancuso@case.edu>
2018-02-27 22:38:14 -0500

    basic layout html css and pseudocode added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TriviaGame
 Javascript trivia game
+
+My take on a trivia game written with javascript/jQuery. Sk8orDie!
---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//start button to initialize
+//click event that randomly chooses a question (slide?)
+//multiple choice timed answer 43 seconds
+//responses:
+//4 divs with correct/incorrect classes
+//if correct answer chosen, log wins++, 
+//show gif, move to next question
+//if else time runs out, alert time's up, log loss++
+//show biff gif, move to next question
+//else user picks wrong, log loss++
+//show biff gif, move to next question
+//final screen displays score
+//show restart option button
